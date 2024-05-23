921473449abfa8f256365d85cb83eb62c149d321
Michaui <michael_franiczek@web.de>
2024-05-23 23:50:19 +0200

    Add Eventhandler and Validation

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,45 +1,74 @@
 import "./style.css";
 
+//.value macht hier kein Sinn, weil zum Zeitpunkt vom Seitenaufruf nichts drin steht. 
+//let locationField = document.getElementById('locationField').value; 
+let locationField = document.getElementById('locationField'); 
+let searchBtn = document.getElementById('searchBtn');
+
+
+searchBtn.addEventListener("click", () => {
+  let locationFieldValue = locationField.value.trim(); 
+  let regex = /^\d+,[a-zA-Z]{2}$/; //Validierung von PLZ Eingabe z.B.: 59555,de
+  /*
+    ^: Der Anker "^" stellt sicher, dass der reguläre Ausdruck am Anfang des Strings passt.
+    \d+: Übereinstimmung mit einer oder mehreren Ziffern.
+    ,: Übereinstimmung mit einem Komma.
+    [a-zA-Z]+: Übereinstimmung mit einem oder mehreren Buchstaben (Groß- oder Kleinbuchstaben).
+    $: Der Anker "$" stellt sicher, dass der reguläre Ausdruck am Ende des Strings passt.
+  */
+
+  //Wenn locationFieldValue Zahlen gefolgt von Komma und Buchstabe ist. 
+  if(regex.test(locationFieldValue) && locationFieldValue !== ''){
+    //getWeatherByZip("47057,de")
+    getWeatherByZip(locationFieldValue); 
+  } 
+  //Wenn locationFieldValue nur Text bzw. Stadt ist 
+  if(/[a-zA-Z]/.test(locationFieldValue)){
+    //getWeather({q:'Mainz'})
+    getWeather({q:locationFieldValue})
+  }
+  if(true){
+    //MUSS NOCH DEFINIERT WERDEN LOG, LON 
+  }
+  else {
+    console.log("SCHEI?E")
+  }
+})
+
+
 // @param {Object.<string, string>} query - An object with string keys and string values.
 async function getWeather(queryPara) {
   const query = new URLSearchParams(queryPara);
 
   query.append("units", "metric");
   query.append("appID", import.meta.env.VITE_OPENWEATHERMAP_API_KEY);
-  const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?${query.toString()}`
+  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?${query.toString()}`
   );
 
-/*
-Wenn das JSON der Funktion übergeben wird, muss das JSON nicht aufgelöst werden,
-weil .then() eine funktion eines promise ist -> Dass die gleiche Funktion hat, wie das promise.
-Siehe Funktionsaufruf: getWeather({q:'Duisburg'}).then(console.log);
+  const dataWeather = await data.json();
+  //return dataWeather; -> Nur notwendig, wenn Funktionsaufruf mit .then(console.log); ausgeführt wird. 
+  //console.log(dataWeather.weather[0].description); 
+  showWeatherData(dataWeather);
+};
 
+/*
+Wenn das JSON der Funktion übergeben wird, muss das JSON nicht aufgelöst werden, weil .then() eine funktion eines promise ist -> Dass die gleiche Funktion hat, wie das promise.
+Siehe Funktionsaufruf: getWeather({q:'Duisburg'}).then(console.log);
 //return data.json();
 
 Hier ist eine Erklärung und ein überarbeitetes Beispiel:
 fetch() ist asynchron: Die fetch-Funktion ruft eine Netzwerkanfrage ab und gibt ein Promise zurück. Wenn die Anfrage abgeschlossen ist, wird das Promise mit einem Response-Objekt aufgelöst.
 Response.json() ist ebenfalls asynchron: Die Methode json() des Response-Objekts liest den Body der Antwort und gibt ebenfalls ein Promise zurück, das mit den geparsten JSON-Daten aufgelöst wird.
 */
-const dataWeather = await data.json()
-
-showWeatherData(dataWeather);
-// console.log(dataWeather.weather[0].description); 
-};
-
-// async function getWeatherByZip(zip){
-//     return getWeather({zip})
-// }
 
 //OBJEKT WRAPPER
 // getWeatherByZip({zip: "94040,us}) => getWeatherByZip("94040,us")
 async function getWeatherByZip(countryCode) {
-  return getWeather({ zip: countryCode });
+  // das "return" ist nur notwendig, wenn der Aufruf der Funktion ein .then folgt, damit die verarbeiteten Daten dargestellt werden können. -> getWeatherByZip("47057,de").then(console.log);
+  // return getWeather({ zip: countryCode });
+  getWeather({ zip: countryCode });
 }
 
-getWeather({q:'Duisburg'});
-// getWeather({lat: "44.43", lon: "10.99"}).then(console.log);
-// getWeatherByZip("47057,de").then(console.log);
 
 function showWeatherData(weatherJSON){
     const json = weatherJSON;
@@ -51,14 +80,19 @@ function showWeatherData(weatherJSON){
     // const currentTempFar = (tempKelvin - 273.15) * 9/5 + 32;
     // console.log(tempKelvin);
 
-    const currentTempCel = weatherJSON.main.temp;
-    const currentTempFar = currentTempCel * 9/5 + 32;
-    console.log(currentTempCel);
-    console.log(currentTempFar.toFixed(2));
-
-    const location = weatherJSON.name;
-    console.log(location);
-
-    const weatherDescription = weatherJSON.weather[0].description;
-    console.log(weatherDescription);
+    const currentTempCel = document.getElementById('tempState').innerText = weatherJSON.main.temp;
+    // Durch die Klammern bei (currentTempCel * 9/5 + 32) besteht die Möglichkeit eine weitere Funktion anzuhängen
+    document.getElementById('tempStateFar').innerText = (currentTempCel * 9/5 + 32).toFixed(2);
+    document.getElementById('locationState').innerText = weatherJSON.name;
+  
+    document.getElementById('weatherState').innerText = weatherJSON.weather[0].description;
 }
+
+
+// getWeather({q:'Mainz'}).then(console.log);
+// getWeather({lat: "44.43", lon: "10.99"}).then(console.log);
+// getWeatherByZip("47057,de").then(console.log);
+
+
+// getWeather({lat: "44.43", lon: "10.99"})
+// 
