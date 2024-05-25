ca17a88b1567fb36a4a5e4d131a0df02ff3a3387
Michaui <michael_franiczek@web.de>
2024-05-25 21:33:02 +0200

    add toggle function and fetch giphy api

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,35 +1,34 @@
 import "./style.css";
-import { init, latitude, longitude, isInitialized } from './location.js';
+import { init, latitude, longitude, isInitialized } from "./location.js";
 
-
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
   init();
-  
+
   // Überprüfe kontinuierlich, ob init() abgeschlossen ist, mit einem Intervall von 100 Millisekunden
   setTimeout(() => {
     if (isInitialized()) {
       console.log(latitude, longitude); // Hier kannst du auf latitude und longitude zugreifen
       // latitude.toFixed(2);
       // longitude.toFixed(2);
-      getWeather({lat: `${latitude}`, lon: `${longitude}`})
+      getWeather({ lat: `${latitude}`, lon: `${longitude}` });
     }
   }, 100);
 });
 
-
 // console.log(latitude, longitude); // Auf die latitude und longitude zugreifen
 
+//.value macht hier kein Sinn, weil zum Zeitpunkt vom Seitenaufruf nichts drin steht.
+//let locationField = document.getElementById('locationField').value;
+let locationField = document.getElementById("locationField");
+locationField.addEventListener("keypress", handleEvent);
 
-//.value macht hier kein Sinn, weil zum Zeitpunkt vom Seitenaufruf nichts drin steht. 
-//let locationField = document.getElementById('locationField').value; 
-let locationField = document.getElementById('locationField'); 
-locationField.addEventListener('keypress', handleEvent); 
+let searchBtn = document
+  .getElementById("searchBtn")
+  .addEventListener("click", handleEvent);
 
-let searchBtn = document.getElementById('searchBtn').addEventListener('click', handleEvent);
-
-function handleEvent(event){
-  if(event.key == 'Enter' || event.type === 'click'){
-    let locationFieldValue = locationField.value.trim(); 
+function handleEvent(event) {
+  if (event.key == "Enter" || event.type === "click") {
+    let locationFieldValue = locationField.value.trim();
     let regex = /^\d+,[a-zA-Z]{2}$/; //Validierung von PLZ Eingabe z.B.: 59555,de
     /*
       ^: Der Anker "^" stellt sicher, dass der reguläre Ausdruck am Anfang des Strings passt.
@@ -38,22 +37,21 @@ function handleEvent(event){
       [a-zA-Z]+: Übereinstimmung mit einem oder mehreren Buchstaben (Groß- oder Kleinbuchstaben).
       $: Der Anker "$" stellt sicher, dass der reguläre Ausdruck am Ende des Strings passt.
     */
-  
-    //Wenn locationFieldValue Zahlen gefolgt von Komma und Buchstabe ist. 
-    if(regex.test(locationFieldValue) && locationFieldValue !== ''){
+
+    //Wenn locationFieldValue Zahlen gefolgt von Komma und Buchstabe ist.
+    if (regex.test(locationFieldValue) && locationFieldValue !== "") {
       //getWeatherByZip("47057,de")
-      getWeatherByZip(locationFieldValue); 
-    } 
-    //Wenn locationFieldValue nur Text bzw. Stadt ist 
-    if(/[a-zA-Z]/.test(locationFieldValue) && locationFieldValue !== ''){
+      getWeatherByZip(locationFieldValue);
+    }
+    //Wenn locationFieldValue nur Text bzw. Stadt ist
+    if (/[a-zA-Z]/.test(locationFieldValue) && locationFieldValue !== "") {
       //getWeather({q:'Mainz'})
-      getWeather({q:locationFieldValue})
+      getWeather({ q: locationFieldValue });
     }
-    if(true){
-      //MUSS NOCH DEFINIERT WERDEN LOG, LON 
-    }
-    else {
-      console.log("SCHEI?E")
+    if (true) {
+      //MUSS NOCH DEFINIERT WERDEN LOG, LON
+    } else {
+      console.log("SCHEI?E");
     }
   }
 }
@@ -64,14 +62,38 @@ async function getWeather(queryPara) {
 
   query.append("units", "metric");
   query.append("appID", import.meta.env.VITE_OPENWEATHERMAP_API_KEY);
-  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?${query.toString()}`
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${query.toString()}`
   );
 
   const dataWeather = await data.json();
-  //return dataWeather; -> Nur notwendig, wenn Funktionsaufruf mit .then(console.log); ausgeführt wird. 
-  //console.log(dataWeather.weather[0].description); 
+  //return dataWeather; -> Nur notwendig, wenn Funktionsaufruf mit .then(console.log); ausgeführt wird.
+  //console.log(dataWeather.weather[0].description);
   showWeatherData(dataWeather);
-};
+}
+
+async function getGiphyImage(searchPara) {
+  // const search = new URLSearchParams(searchPara); HIER
+
+  // search.append("q:", searchPara); HIER
+  // search.append("api_key:", import.meta.env.VITE_GIPHY_API_KEY); HIER
+
+  // const giphyData = await fetch(`api.giphy.com/v1/gifs/search${searchPara.toString()}`).then(response => response.json())
+  // .then(console.log)
+
+  //   const test = await fetch(`https://api.giphy.com/v1/gifs/search/${search.toString()}`); HIER
+  // console.log(test);
+
+  const giphyData = await fetch(
+    `https://api.giphy.com/v1/gifs/search?q=${searchPara}&api_key=${
+      import.meta.env.VITE_GIPHY_API_KEY
+    }`
+  );
+
+  const giphyResponse = await giphyData.json();
+
+  console.log(giphyResponse);
+}
 
 /*
 Wenn das JSON der Funktion übergeben wird, muss das JSON nicht aufgelöst werden, weil .then() eine funktion eines promise ist -> Dass die gleiche Funktion hat, wie das promise.
@@ -91,24 +113,42 @@ async function getWeatherByZip(countryCode) {
   getWeather({ zip: countryCode });
 }
 
+const toggleSwitch = document.querySelector(".toggleSwitch");
+let tempStateFar;
+let tempState;
+const status = document.getElementById("status");
 
-function showWeatherData(weatherJSON){
-    const json = weatherJSON;
-    console.log(json);
+function showWeatherData(weatherJSON) {
+  const json = weatherJSON;
+  console.log(json);
 
-    // Nicht mehr notwending, weil in der URL optional unit:metric als default definiert wird. 
-    // const tempKelvin = weatherJSON.main.temp;
-    // const currentTempCel = tempKelvin - 273.15; 
-    // const currentTempFar = (tempKelvin - 273.15) * 9/5 + 32;
-    // console.log(tempKelvin);
+  // Nicht mehr notwending, weil in der URL optional unit:metric als default definiert wird.
+  // const tempKelvin = weatherJSON.main.temp;
+  // const currentTempCel = tempKelvin - 273.15;
+  // const currentTempFar = (tempKelvin - 273.15) * 9/5 + 32;
+  // console.log(tempKelvin);
 
-    const currentTempCel = document.getElementById('tempState').innerText = weatherJSON.main.temp;
-    // Durch die Klammern bei (currentTempCel * 9/5 + 32) besteht die Möglichkeit eine weitere Funktion anzuhängen
-    document.getElementById('tempStateFar').innerText = (currentTempCel * 9/5 + 32).toFixed(2);
-    document.getElementById('locationState').innerText = weatherJSON.name;
-  
-    document.getElementById('weatherState').innerText = weatherJSON.weather[0].description;
+  tempState = weatherJSON.main.temp;
+  status.textContent = `${tempState} Celsius`;
+  // Durch die Klammern bei (currentTempCel * 9/5 + 32) besteht die Möglichkeit eine weitere Funktion anzuhängen
+  tempStateFar = ((tempState * 9) / 5 + 32).toFixed(2);
+
+  document.getElementById("locationState").innerText = weatherJSON.name;
+
+  document.getElementById("weatherState").innerText =
+    weatherJSON.weather[0].description;
+
+  //GIPHY API ANFRAGE
+  getGiphyImage(weatherJSON.weather[0].description);
 }
+
+toggleSwitch.addEventListener("change", function () {
+  if (this.checked) {
+    status.textContent = `${tempStateFar} Farenheit`;
+  } else {
+    status.textContent = `${tempState} Celsius`;
+  }
+});
 
 // getWeather({q:'Mainz'}).then(console.log);
 // getWeather({lat: "44.43", lon: "10.99"}).then(console.log);
---
+++ WeatherApp/vite.config.js
@@ -0,0 +1,13 @@
+export default {
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+        //   target: "http://127.0.0.1:8000",
+          target: "http://localhost:5173/",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        },
+      },
+    },
+  };
